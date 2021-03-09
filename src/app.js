47c227dcb3fb81d413ac5dc88f6f30a855b383e8
joanne9989 <1619878217@qq.com>
2021-03-09 14:05:11 +0800

    trying express with winter navbar

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var aboutRouter = require('./routes/about');
+var servicesRouter = require('./routes/services');
+var contactRouter = require('./routes/contact');
 
 var app = express();
 
@@ -13,9 +14,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public', express.static(path.join(__dirname, "public")));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/about', aboutRouter);
+app.use('/services', servicesRouter);
+app.use('/contact', contactRouter);
 
 module.exports = app;
